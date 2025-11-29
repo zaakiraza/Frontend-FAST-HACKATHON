a6962a5e052dd85360a0bf4ef938f902c3660868
zaakiraza <raaza.zaakir@gmail.com>
2025-11-29 18:06:46 +0500

    adds

--- Backend/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/dashboardController');
-
-// Dashboard routes
-router.get('/stats', DashboardController.getDashboardStats);
-router.get('/alerts', DashboardController.getRecentAlerts);
-
-module.exports = router;
